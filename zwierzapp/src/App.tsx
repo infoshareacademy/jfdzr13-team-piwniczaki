f3944ce4e9e1ab6d0644babc4107d7b41f0f7372
Robert Krzak <robert.krzaku@gmail.com>
2024-05-26 13:11:33 +0200

    create vite app + scss

---
+++ zwierzapp/src/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles/app.module.scss";
+
+function App() {
+  return (
+    <>
+      {/* Przykładowy nagłowek do testu scss */}
+      <h1 className={styles.header}>Zwierzapp</h1>
+    </>
+  );
+}
+
+export default App;
