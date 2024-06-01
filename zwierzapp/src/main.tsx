db08f299db106f7b49dc6ced04bd9095907a6378
Mateusz Wasinski <mateuszwasinski90@gmail.com>
2024-06-01 11:56:49 +0200

    added react router with basic settings

--- zwierzapp/src/main.tsx
+++ zwierzapp/src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
