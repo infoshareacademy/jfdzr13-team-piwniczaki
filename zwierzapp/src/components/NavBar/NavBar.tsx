93b20ccb2e7bc39226feafc7ab2ce27f71e339e3
EMILIA <emilia.bechta2912@gmail.com>
2024-06-03 19:18:52 +0200

    NavBar component and CSS

--- zwierzapp/src/components/NavBar/NavBar.tsx
+++ zwierzapp/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,19 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/paw.png";
 import styles from "./navBar.module.scss";
 
 function NavBar() {
   return (
-    <div className={styles.navbarContainer}>NavBar</div>
-  )
+    <nav className={styles.navbarContainer}>
+      <div className={styles.navLeft}>
+        <div>ZWIERZAPP</div>
+        <img className={styles.navLogo} src={logo} alt="logo łapy"></img>
+      </div>
+      <NavLink to="/login">
+        <button className={styles.navLoginButton}>ZALOGUJ SIĘ</button>
+      </NavLink>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
