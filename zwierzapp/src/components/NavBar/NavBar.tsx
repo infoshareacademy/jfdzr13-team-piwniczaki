16130029c04b48cd0b2be39e76370bb3397423b0
Mateusz Wasinski <mateuszwasinski90@gmail.com>
2024-06-06 17:23:34 +0200

    NavBar added and styled

--- zwierzapp/src/components/NavBar/NavBar.tsx
+++ zwierzapp/src/components/NavBar/NavBar.tsx
@@ -1,8 +1,35 @@
 import styles from "./navBar.module.scss";
+import paw from "../../images/Paw.svg"
+import { Link } from "react-router-dom";
+import useAuth from "../../context/AuthContext"
 
 function NavBar() {
+  const { currentUser, logout } = useAuth();
   return (
-    <div className={styles.navbarContainer}>NavBar</div>
+    <div className={styles.navbarContainer}>
+      <div className={styles.logoAndNameNav}>
+      <Link to="/" className={styles.nameApp}>ZWIERZAPP
+        <img src={paw}></img></Link>
+      </div>
+      <div className={styles.menuNav}>
+        <Link to="/#aboutUs" className={styles.linkElemenets}>O nas</Link>
+        <Link to="/#contact" className={styles.linkElemenets}>Kontakt</Link>
+
+        {currentUser ? (
+          <>
+            <Link to="/profile" className={styles.linkElemenets}>Profil</Link>
+            <Link to="/filter" className={styles.linkElemenets}>Znajdź Petsittera</Link>
+            <Link to="/" className={styles.linkElemenets} onClick={logout}>Wyloguj się</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className={styles.linkElemenets}>Zaloguj się</Link>
+            <Link to="/register" className={styles.linkElemenets}>Zarejestruj się</Link>
+          </>
+        )}
+      </div>
+    </div>
+
   )
 }
 
--- zwierzapp/src/context/AuthContext.tsx
+++ zwierzapp/src/context/AuthContext.tsx
@@ -1,12 +1,37 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
 import { auth } from "../utils/firebase.js";
-import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = createContext({});
+
 const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+
     const [currentUser, setCurrentUser] = useState(null);
+
+    function login(email: string, password: string) {
+        signInWithEmailAndPassword(auth, email, password)
+
+    }
+    function loginWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider)
+
+    }
+    function register(email: string, password: string) {
+        createUserWithEmailAndPassword(auth, email, password);
+    }
+    function logout() {
+        signOut(auth);
+    }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -15,9 +40,16 @@ export const AuthProvider = ({ children }) => {
         return () => unsubscribe();
     }, []);
 
+    const passedData = {
+        currentUser,
+        login,
+        loginWithGoogle,
+        register,
+        logout
+    }
 
-    return <AuthContext.Provider value={currentUser}>{children}</AuthContext.Provider>
 
+    return <AuthContext.Provider value={passedData}>{children}</AuthContext.Provider>
 }
 
 export default useAuth;
