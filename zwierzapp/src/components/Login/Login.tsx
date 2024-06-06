4b5c8dcb939c3c5ace3f19efcbf9328adf218935
Mateusz Wasinski <mateuszwasinski90@gmail.com>
2024-06-06 12:45:54 +0200

    added styles to login component

--- zwierzapp/src/components/Login/Login.tsx
+++ zwierzapp/src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import styles from "./login.module.scss";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import useAuth from "../../context/AuthContext"
 import googleLogo from "../../images/googleLogo.svg"
@@ -7,7 +7,7 @@ import arrowLeft from "../../images/arrow-left.svg"
 
 
 const Login = () => {
-	const { loginWithGoogle, login, currentUser } = useAuth();
+  const { loginWithGoogle, login, currentUser } = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -33,14 +33,17 @@ const Login = () => {
   return (
     <div className={styles.loginContainer}>
       <div className={styles.boxLogin}>
-        <Link to="/" className={styles.linkToMainPage}> <img className={styles.arrowLeft} src={arrowLeft} />WRÓĆ NA STRONĘ GŁÓWNĄ</Link>
-        <h1>S I E M A N K O</h1>
+        <h1>WITAJ!</h1>
         <form onSubmit={handleSubmit} className={styles.formContainer}>
           <input type="email" name="email" placeholder="ADRES MAIL" required />
           <input type="password" name="password" placeholder="HASŁO" required />
           <button type="submit">ZALOGUJ SIĘ</button>
         </form>
-        <p>LUB</p>
+        <div className={styles.orContainer}>
+          <div className={styles.lineElement}></div>
+          <p>LUB</p>
+          <div className={styles.lineElement}></div>
+        </div>
         <button className={styles.googleLogin} onClick={loginWithGoogle}><img className={styles.googleLogo} src={googleLogo} />KONTYNUUJ Z UŻYCIEM KONTA GOOGLE</button>
         <Link to="/register" className={styles.registerLink}>NIE MASZ KONTA? ZAREJESTRUJ SIĘ!</Link>
       </div>
