8c6dfa3a5069864fa62fe0c80a8c68029b81a31b
Mateusz Wasinski <mateuszwasinski90@gmail.com>
2024-06-09 11:10:32 +0200

    fixed google auth in register and login component

--- zwierzapp/src/components/MainPage/MainPage.tsx
+++ zwierzapp/src/components/MainPage/MainPage.tsx
@@ -1,18 +1,18 @@
 
 import WelcomePage from "./WelcomePage/WelcomePage";
-import Carousel  from "./Carousel/Carousel";
+import Carousel from "./Carousel/Carousel";
 import AboutUs from "./AboutUs/AboutUs";
 import Footer from "./Footer/Footer";
 
 function MainPage() {
-return (
-<>
-<WelcomePage/>
-<Carousel/>
-<AboutUs/>
-<Footer/>
-</>
-)
+    return (
+        <>
+            <WelcomePage />
+            <Carousel />
+            <AboutUs />
+            <Footer />
+        </>
+    )
 }
 
 export default MainPage;
