d0a9b26c4789bfa00c1378a814da5b540149dc55
Mateusz Wasinski <mateuszwasinski90@gmail.com>
2024-06-14 09:52:35 +0200

    AddPet component, styles, sending from data to firebase added

--- zwierzapp/src/components/AddPet/AddPet.tsx
+++ zwierzapp/src/components/AddPet/AddPet.tsx
@@ -1,8 +1,23 @@
 import styles from "./addPet.module.scss";
+import { Link } from "react-router-dom";
+import InputDataPet from "./InputDataPet/InputDataPet";
+import arrowback from "../../images/arrow_back.svg"
 
 function AddPet() {
+
+
+
   return (
-    <div className={styles.addpetContainer}>AddPet</div>
+    <div className={styles.addpetContainer}>
+
+      <div className={styles.containerForm}>
+        <div className={styles.getBackContainer}>
+          <Link to="/" className={styles.getBackElement}><img src={arrowback}></img>wróć</Link>
+        </div>
+        <InputDataPet/>
+        <Link to="/" className={styles.buttonToCancel} >Anuluj</Link>
+      </div>
+    </div>
   )
 }
 
