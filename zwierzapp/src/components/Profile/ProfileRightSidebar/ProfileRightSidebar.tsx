4dbc795e6b57b82cf59e6c01fec3c4ec8c00dc37
Barbara Szymańska <basia.szymanska.01@gmail.com>
2024-06-16 12:35:56 +0200

    Create general layout for Profil component

---
+++ zwierzapp/src/components/Profile/ProfileRightSidebar/ProfileRightSidebar.tsx
@@ -0,0 +1,11 @@
+import styles from "./ProfileRightSidebar.module.scss";
+
+function ProfileRightSidebar() {
+  return (
+    <div className={styles.container}>
+      ProfileRightSidebar
+    </div>
+  )
+}
+
+export default ProfileRightSidebar
