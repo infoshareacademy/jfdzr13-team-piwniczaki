89443e1124874f23eca2fdd059e1b5a881030b56
Mateusz Wasinski <mateuszwasinski90@gmail.com>
2024-06-21 11:10:17 +0200

    adding fature with hiding burger menu; adding styles

--- zwierzapp/src/components/NavBar/NavProfile/NavProfile.tsx
+++ zwierzapp/src/components/NavBar/NavProfile/NavProfile.tsx
@@ -3,42 +3,46 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import useAuth from "../../../context/AuthContext";
 
-import profileImg from "../../../images/Profile button.png"
-import settingIcon from "../../../images/settings.svg"
-import findIcon from "../../../images/person_search.svg"
+import profileImg from "../../../images/Profile button.png";
+import settingIcon from "../../../images/settings.svg";
+import findIcon from "../../../images/person_search.svg";
 
 function NavProfile() {
     const { currentUser, logout } = useAuth() || {};
-    const [ isClicked, setClick ] = useState(false);
+    const [isClicked, setClick] = useState(false);
 
     function showMenu() {
-        if (isClicked) {
-            setClick(false)
-        } else {
-            setClick(true)
-        }
+        setClick((prevState) => !prevState);
+    }
+
+    function hideMenu() {
+        setClick(false);
     }
 
     return (
         <div className={styles.profileNavBarContainer}>
-            <img src={profileImg} onClick={showMenu}></img>
-            {isClicked ? (
-                <div className={styles.hamburgerMenu}>
+            <img src={profileImg} onClick={showMenu} alt="Profile" />
+            {isClicked && (
+                <div
+                    className={styles.hamburgerMenu}
+                    onMouseLeave={hideMenu}
+                >
                     <div className={styles.cornerBox}></div>
                     <div className={styles.userBox}>
-                        <span className={styles.userName}>{currentUser.displayName}</span>
+                        <span className={styles.userName}>{currentUser.name} {currentUser.surname}</span>
                         <span className={styles.userMail}>{currentUser.email}</span>
                     </div>
-                    <Link to="/profile" className={styles.settingButton}><img src={settingIcon} />Ustawienia konta</Link>
-                    <Link to="/filter" className={styles.settingButton}><img src={findIcon} />Znajdź Petsittera</Link>
+                    <Link to="/profile" className={styles.settingButton}>
+                        <img src={settingIcon} alt="Settings" />Ustawienia konta
+                    </Link>
+                    <Link to="/filter" className={styles.settingButton}>
+                        <img src={findIcon} alt="Find Petsitter" />Znajdź Petsittera
+                    </Link>
                     <Link to="/" className={styles.logoutButton} onClick={logout}>Wyloguj się</Link>
                 </div>
-            ) : (
-                <></>
             )}
-
         </div>
-    )
+    );
 }
 
-export default NavProfile
+export default NavProfile;
