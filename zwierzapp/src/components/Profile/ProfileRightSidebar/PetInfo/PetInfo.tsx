dc2c7aafb2ba30149352ce09401a6e19ebf243a9
Barbara Szymańska <basia.szymanska.01@gmail.com>
2024-06-26 21:30:13 +0200

    Add skeleton of profileAsidePet component with comments

--- zwierzapp/src/components/Profile/ProfileRightSidebar/PetInfo/PetInfo.tsx
+++ zwierzapp/src/components/Profile/ProfileRightSidebar/PetInfo/PetInfo.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const PetInfo = () => {
+
+function PetInfo() {
+
+const [userHasPets, setUserHasPets] = useState(null)
+
+useEffect(()=>{
+  //tutaj sprawdzam czy w kolekcji pets istnieje dokument, który zawiera userId = currentUser.uid
+  //jeśli tak to ustawiam useState na true, jeśli nie to na false
+})
   return (
+//jeśli userHasPets jest true tozaciągam dane z bazy i wyświetlam imię zwierzaka/zwierzaków, które są linkami do formularza addPet
+//jeśli userHasPets jest false lub falsy to wyświetlam przycisk dodaj zwierzaka, który przekierowuje na formularz addPet
     <div>
-      petinfo
-    </div>
-  )
+      {userHasPets ? 
+      (<div>
+    
+      </div>)
+      :
+      (<div>
+
+      </div>)}
+    </div>  
+   )
 }
 
 export default PetInfo
