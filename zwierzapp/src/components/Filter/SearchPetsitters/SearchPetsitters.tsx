2591372a7f0bcf3162706169c4dc8e2d8d45800b
Barbara Szymańska <basia.szymanska.01@gmail.com>
2024-06-30 08:28:35 +0200

    Multi-range-slider package added to SearchPetsitter

--- zwierzapp/src/components/Filter/SearchPetsitters/SearchPetsitters.tsx
+++ zwierzapp/src/components/Filter/SearchPetsitters/SearchPetsitters.tsx
@@ -1,104 +1,16 @@
-// import React, { useState } from 'react'
-// import styles from './SearchPetsitters.module.scss'
-// import getPetData from '../../../hooks/getPetData'
-// import useAuth from '../../../context/AuthContext'
-// import MultiRangeSlider from "multi-range-slider-react";
-
-// const SearchPetsitters = () => {
-//   const [formData, setFormData] = useState({
-//     petName: '',
-//     serviceType: '',
-//     city: '',
-//     startDate: '',
-//     endDate: '',
-//     priceRange: {
-//       minValue: 0,
-//       maxValue: 200
-//     }
-//   });
-//   const {currentUser} = useAuth() || {}
-//   const petsArr = getPetData(currentUser.uid)
-//   const [minValue, set_minValue] = useState(0);
-//   const [maxValue, set_maxValue] = useState(200)
-
-//   const handlePriceChange = (e: { minValue: number; maxValue: number }) => {
-//     setFormData({
-//       ...formData,
-//       priceRange: {
-//         minValue: e.minValue,
-//         maxValue: e.maxValue
-//       }
-//     });
-//   };
-
-//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-
-   
-//     console.log(formData);
-//   };
-
-//   return (
-// <div className={styles.pageContainer}>
-//   <h1>Wyszukaj opiekuna</h1>
-//   <form className={styles.form} onSubmit={handleSubmit}>
-
-//     <label>
-//       Imię zwierzaka
-//       <select>
-//         {petsArr.length >= 1 && petsArr.map((pet)=> (<option key={pet.id}>{pet.name}</option>))}
-//       </select>
-//     </label>
-
-//     <label>
-//       Rodzaj usługi
-//       <label><input type='radio' name='offer' value='walk'/> Spacer </label>
-//       <label><input type='radio' name='offer' value='accom'/> Nocleg </label>
-//       <label><input type='radio' name='offer' value='homeVisit'/> Wizyta domowa </label>
-//     </label>
-
-//     <label>
-//       Miasto
-//       <input type='text'/>
-//     </label>
-//     <label>
-//       Termin
-//       <input type='date' name='startDate'/>
-//       <input type='date' name='endDate'/>
-//     </label>
-
-//     <label>
-//       Cena
-//       <MultiRangeSlider
-// 			min={0}
-// 			max={200}
-// 			step={10}
-// 			minValue={minValue}
-// 			maxValue={maxValue}
-// 			onInput={(e) => {
-// 				handleInput(e);
-// 			}}
-// 		/>
-//     </label>
-//   </form>
-//   <button type='submit'>Wyszukaj</button>
-// </div>
-//   )
-// }
-
-// export default SearchPetsitters
 
 import React, { useState } from 'react';
 import styles from './SearchPetsitters.module.scss';
 import getPetData from '../../../hooks/getPetData';
 import useAuth from '../../../context/AuthContext';
 import MultiRangeSlider from 'multi-range-slider-react';
+import { useParams } from 'react-router';
 
 const SearchPetsitters = () => {
   const { currentUser } = useAuth() || {};
   const petsArr = getPetData(currentUser.uid);
 
-  // Stan do przechowywania danych formularza
+
   const [formData, setFormData] = useState({
     petName: '',
     serviceType: '',
@@ -109,7 +21,7 @@ const SearchPetsitters = () => {
     maxValue: 200
   });
 
-  // Funkcja obsługująca zmianę wartości w formularzu dla  suwaka
+useParams
   const handlePriceChange = (e: { minValue: number; maxValue: number }) => {
     setFormData({
       ...formData,
