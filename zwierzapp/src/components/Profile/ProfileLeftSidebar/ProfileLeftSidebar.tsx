156d42c8fe2ad9b0907a72aca24c8c819c02dc04
EMILIA <emilia.bechta2912@gmail.com>
2024-06-30 08:16:51 +0200

    Add edit, save functionality to ProfileLeftSidebar component

--- zwierzapp/src/components/Profile/ProfileLeftSidebar/ProfileLeftSidebar.tsx
+++ zwierzapp/src/components/Profile/ProfileLeftSidebar/ProfileLeftSidebar.tsx
@@ -4,72 +4,117 @@ import styles from "./ProfileLeftSidebar.module.scss";
 
 const ProfileLeftSidebar = () => {
   const { currentUser, savePersonalData } = useAuth() || {};
-  console.log(currentUser);
+  const [isEditing, setIsEditing] = useState(false);
+  // AWATAR
+  // const [avatar, setAvatar] = useState(null);
 
   const [inputs, setInputs] = useState({
-    name: currentUser.name, // ostatni etap
+    name: currentUser.name,
     surname: currentUser.surname,
     phone: currentUser.phone,
     city: currentUser.city,
+    shortDescription: currentUser.shortDescription,
   });
 
   const handleUpdateData = async (e) => {
     e.preventDefault();
-    await savePersonalData(inputs); // w momencie jak ktoś kliknie zapisz
+    await savePersonalData(inputs);
+    setIsEditing(false);
   };
 
   return (
     <>
       <div className={styles.container}>
-        ProfileLeftSidebar
-        <h1>UZUPEŁNIJ DANE</h1>
+          {isEditing ? (
+
         <form className={styles.formContainer} onSubmit={handleUpdateData}>
-          {/* jak zaktualizuję "zapiszę" */}
-          <input
-            type="text"
-            name="name"
-            placeholder="IMIĘ"
-            required
-            value={inputs.name}
-            onChange={
-              (e) => setInputs((prev) => ({ ...prev, name: e.target.value })) // kopiuję wartości obiektu/wszystkich inputów i ustawiam na nowo w zależnosci od danych pobranych
-            }
-          />
-          <input
-            autoComplete="on"
-            type="text"
-            name="surname"
-            placeholder="NAZWISKO"
-            required
-            value={inputs.surname}
-            onChange={(e) =>
-              setInputs((prev) => ({ ...prev, surname: e.target.value }))
-            }
-          />
-          <input
-            autoComplete="on"
-            type="tel"
-            name="phone"
-            placeholder="NUMER TELEFONU"
-            required
-            value={inputs.phone}
-            onChange={(e) =>
-              setInputs((prev) => ({ ...prev, phone: e.target.value }))
-            }
-          />
-          <input
-            autoComplete="on"
-            type="text"
-            name="city"
-            placeholder="MIASTO"
-            required
-            value={inputs.city}
-            onChange={(e) =>
-              setInputs((prev) => ({ ...prev, city: e.target.value }))
-            }
-          />
-          <button type="submit">ZAPISZ ZMIANY</button>
-        </form>
+              <textarea
+                className={styles.shortDescription}
+                id="shortDescription"
+                name="shortDescription"
+                value={inputs.shortDescription}
+                placeholder="Dodaj krótki opis"
+                onChange={(e) =>
+                  setInputs((prev) => ({
+                    ...prev,
+                    shortDescription: e.target.value,
+                  }))
+                }
+              ></textarea>
+              <input
+                type="text"
+                name="name"
+                placeholder="IMIĘ"
+                required
+                value={inputs.name}
+                onChange={(e) =>
+                  setInputs((prev) => ({ ...prev, name: e.target.value }))
+                }
+              />
+              <input
+                autoComplete="on"
+                type="text"
+                name="surname"
+                placeholder="NAZWISKO"
+                required
+                value={inputs.surname}
+                onChange={(e) =>
+                  setInputs((prev) => ({ ...prev, surname: e.target.value }))
+                }
+              />
+              <input
+                autoComplete="on"
+                type="tel"
+                name="phone"
+                placeholder="NUMER TELEFONU"
+                required
+                value={inputs.phone}
+                onChange={(e) =>
+                  setInputs((prev) => ({ ...prev, phone: e.target.value }))
+                }
+              />
+              <input
+                autoComplete="on"
+                type="text"
+                name="city"
+                placeholder="MIASTO"
+                required
+                value={inputs.city}
+                onChange={(e) =>
+                  setInputs((prev) => ({ ...prev, city: e.target.value }))
+                }
+              />
+              <button className={styles.saveChangesButton} type="submit">
+                Zapisz zmiany
+              </button>
+              </form>
+
+          ) : (
+            <>
+              <p>
+                <strong>IMIĘ:</strong> {inputs.name}
+              </p>
+              <p>
+                <strong>NAZWISKO:</strong> {inputs.surname}
+              </p>
+              <p>
+                <strong>NUMER TELEFONU:</strong> {inputs.phone}
+              </p>
+              <p>
+                <strong>MIASTO:</strong> {inputs.city}
+              </p>
+              <p>
+                <strong>OPIS:</strong> {inputs.shortDescription}
+              </p>
+              <button
+                className={styles.editButton}
+                type="button"
+                onClick={() => setIsEditing(true)}
+              >
+                Edytuj dane
+              </button>
+            </>
+          )}
       </div>
     </>
   );
