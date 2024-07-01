ada3002e42d0f8e57638f09fc037739cbbb31558
Barbara Szymańska <basia.szymanska.01@gmail.com>
2024-07-01 20:47:54 +0200

    Add validation to date and price, add default values -not finished

--- zwierzapp/src/components/Filter/SearchPetsitters/SearchPetsitters.tsx
+++ zwierzapp/src/components/Filter/SearchPetsitters/SearchPetsitters.tsx
@@ -7,43 +7,45 @@ interface FormField {
   [key: string]: string | number;
 }
 
-const initialFormData: FormField = {
-   petId: "",
-   serviceType: "",
-   city: "",
-   startDate: "",
-   endDate: "",
-   minPrice: 1,
-   maxPrice: 200,
-};
-
 
 
 
 const SearchPetsitters = () => {
-  const [formData, setFormData] = useState<FormField>(initialFormData);
-  const [searchParams, setSearchParams] = useSearchParams();
   const { currentUser } = useAuth() || {};
   const petsArr = getPetData(currentUser.uid);
   const currentDate = new Date().toISOString().split("T")[0];
-
-
-
+  const nextDate = new Date(currentDate);
+  nextDate.setDate(nextDate.getDate() + 1);
+  const nextDateString = nextDate.toISOString().split("T")[0];
+  
+  const initialFormData: FormField = {
+    petId:  petsArr.length >= 1 ? petsArr[0].id : "",
+    serviceType: "walk",
+    city: currentUser.city,
+    startDate: currentDate,
+    endDate: nextDateString,
+    minPrice: 10,
+    maxPrice: 200,
+  };
+  
+  const [formData, setFormData] = useState<FormField>(initialFormData);
+  const [searchParams, setSearchParams] = useSearchParams();
+  
   //ustawienie formy parametrów przy pierwszym i kolejnych parametrach
   useEffect(() => {
     let initialSearchQuery = "";
 
+    console.log('heja z useEffect formData - domyślne',formData)
     Object.keys(formData).forEach((paramName) => {
       if (formData[paramName]) {
         if (initialSearchQuery) {
           initialSearchQuery += `&${paramName}=${formData[paramName]}`;
         } else {
           initialSearchQuery += `${paramName}=${formData[paramName]}`;
-          //tutaj można ustawić wartości domyślne
         }
       }
     });
-
+    console.log('heja z search params w useEffect',searchParams)
     setSearchParams(initialSearchQuery);
   }, []);
 
@@ -75,13 +77,11 @@ const SearchPetsitters = () => {
   // }, []);
   
   
-
-
-
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = e.target;
+    console.log("handleChange - name:", name, "value:", value);
     setFormData({ ...formData, [name]: value });
   };
 
@@ -89,10 +89,12 @@ const SearchPetsitters = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formValues = new FormData(e.target as HTMLFormElement);
-
     let searchQuery = "";
 
     formValues.entries().forEach(([paramName, paramValue]) => {
+      if (paramName === "city" && typeof paramValue === "string") {
+        paramValue = paramValue.trim().toLowerCase().replace(/\s+/g, "-");
+      }
       if (paramValue) {
         if (searchQuery) {
           searchQuery += `&${paramName}=${paramValue}`;
@@ -122,117 +124,122 @@ const SearchPetsitters = () => {
   };
 
   return (
-    <div className={styles.pageContainer}>
-      <h1>Wyszukaj opiekuna</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <label>
-          Imię zwierzaka
-          <select name="petId" onChange={handleChange}>
-            {petsArr.length >= 1 &&
-              petsArr.map((pet) => (
-                <option key={pet.id} value={pet.id}>
-                  {pet.name}
-                </option>
-              ))}
-          </select>
-        </label>
+    <div>
+      {petsArr && 
+        <div className={styles.pageContainer}>
+          <h1>Wyszukaj opiekuna</h1>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <label>
+              Imię zwierzaka
+              <select name="petId" defaultValue={formData.petId} onChange={handleChange}>
+                {petsArr.length >= 1 &&
+                  petsArr.map((pet) => (
+                    <option key={pet.id} value={pet.id}>
+                      {pet.name}
+                    </option>
+                  ))}
+              </select>
+            </label>
 
-        <label>
-          Rodzaj usługi
-          <label>
-            <input
-              type="radio"
-              name="serviceType"
-              value="walk"
-              onChange={handleChange}
-              checked={formData.serviceType === "walk"}
-            />{" "}
-            Spacer{" "}
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="serviceType"
-              value="accom"
-              onChange={handleChange}
-              checked={formData.serviceType === "accom"}
-            />{" "}
-            Nocleg{" "}
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="serviceType"
-              value="homeVisit"
-              onChange={handleChange}
-              checked={formData.serviceType === "homeVisit"}
-            />{" "}
-            Wizyta domowa{" "}
-          </label>
-        </label>
+            <label>
+              Rodzaj usługi
+              <label>
+                <input
+                  type="radio"
+                  name="serviceType"
+                  value="walk"
+                  onChange={handleChange}
+                  checked={formData.serviceType === "walk"}
+                />{" "}
+                Spacer{" "}
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="serviceType"
+                  value="accom"
+                  onChange={handleChange}
+                  checked={formData.serviceType === "accom"}
+                />{" "}
+                Nocleg{" "}
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="serviceType"
+                  value="homeVisit"
+                  onChange={handleChange}
+                  checked={formData.serviceType === "homeVisit"}
+                />{" "}
+                Wizyta domowa{" "}
+              </label>
+            </label>
 
-        <label>
-          Miasto
-          <input
-            type="text"
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-          />
-        </label>
+            <label>
+              Miasto
+              <input
+                type="text"
+                name="city"
+                value={formData.city}
+                onChange={handleChange}
+              />
+            </label>
 
-        <label>
-          Termin
-          <input
-            type="date"
-            name="startDate"
-            value={formData.startDate}
-            min={currentDate}
-            onChange={handleChange}
-          />
-          <input
-            type="date"
-            name="endDate"
-            value={formData.endDate}
-            min={currentDate}
-            onChange={handleChange}
-          />
-        </label>
+            <label>
+              Termin
+              <input
+                type="date"
+                name="startDate"
+                value={formData.startDate}
+                min={currentDate}
+                max={formData.endDate}
+                onChange={handleChange}
+              />
+              <input
+                type="date"
+                name="endDate"
+                value={formData.endDate}
+                min={formData.startDate}
+                onChange={handleChange}
+              />
+            </label>
 
-        <label>
-          Cena
-          {/* <MultiRangeSlider
-            min={0}
-            max={200}
-            step={10}
-            minValue={formData.minValue}
-            maxValue={formData.maxValue}
-            onInput={handlePriceChange}
-          /> */}
-          <label>
-            min
-            <input
-              type="number"
-              name="minPrice"
-              value={formData.minPrice}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            max
-            <input
-              type="number"
-              name="maxPrice"
-              value={formData.maxPrice}
-              onChange={handleChange}
-            />
-          </label>
-        </label>
+            <label>
+              Cena
+              {/* <MultiRangeSlider
+                min={0}
+                max={200}
+                step={10}
+                minValue={formData.minValue}
+                maxValue={formData.maxValue}
+                onInput={handlePriceChange}
+              /> */}
+              <label>
+                min
+                <input
+                  type="number"
+                  name="minPrice"
+                  value={formData.minPrice}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                max
+                <input
+                  type="number"
+                  name="maxPrice"
+                  value={formData.maxPrice}
+                  min={formData.minPrice}
+                  onChange={handleChange}
+                />
+              </label>
+            </label>
 
-        <button type="submit">Szukaj</button>
-      </form>
-    </div>
-  );
-};
+            <button type="submit">Szukaj</button>
+          </form>
+        </div>
+      }
+  </div>
+)};
 
 export default SearchPetsitters;
