0bca21f1911e9de7a9ddde35021420ca934e7ed4
Barbara Szymańska <basia.szymanska.01@gmail.com>
2024-07-07 09:54:26 +0200

    add email func to button

--- zwierzapp/src/components/Filter/PetsitterBar/PetsitterBar.tsx
+++ zwierzapp/src/components/Filter/PetsitterBar/PetsitterBar.tsx
@@ -58,7 +58,66 @@ const PetsitterBar: React.FC<PetsitterBarProps> = ({
                 <span className={styles.sum}>suma</span>
                 <span className={styles.amount}>{` ${getPriceForService(petsitter)} zł`}</span>
               </div>
-              <button className={`${styles.askButton} ${styles.primaryButton}`}>Wyślij zapytanie</button>
+              {petsitter?.userData?.email ? (
+                <button className={`${styles.askButton} ${styles.primaryButton}`}>
+                  <a
+                    href={`mailto:${petsitter?.userData?.email}?subject=${
+                      filters?.petName
+                    } poszukuje opieki! | Zwierzapp&body=Cześć ${
+                      petsitter?.userData?.name
+                    }!%0D%0A%0D%0A${
+                      filters?.petName
+                    } portrzebuje Twojej opieki w terminie od ${
+                      filters?.startDate
+                    } do ${
+                      filters?.endDate
+                    }%0D%0ASkontaktuj się z właścicielem jak najszybciej, jezeli jesteś zainteresowany opieką.%0D%0A %0D%0A Dane opiekuna: %0D%0A${
+                      currentUser?.name
+                    } ${currentUser?.surname}%0D%0AAdres mail: ${
+                      currentUser?.email
+                    }%0D%0ANumer telefonu: ${
+                      currentUser?.phone
+                    }%0D%0ARodzaj usługi: ${
+                      filters?.serviceType === "accom"
+                        ? "Nocleg"
+                        : filters?.serviceType === "walk"
+                        ? "Spacer"
+                        : filters?.serviceType === "homeVisit"
+                        ? "Wizyta domowa"
+                        : "Nie wybrano"
+                    }%0D%0ALokalizacja usługi: ${
+                      filters?.city.charAt(0).toUpperCase() +
+                      filters?.city.substring(1)
+                    }%0D%0ATermin od ${filters?.startDate} do ${
+                      filters?.endDate
+                    }
+                  
+                  %0D%0A%0D%0ADane zwierzaka:%0D%0AImię: ${
+                    filters?.petName
+                  }%0D%0APłeć: ${
+                      filters?.petSex === "male" ? "Samiec" : "Samica"
+                    }%0D%0ARasa: ${
+                      filters?.petRace === "dog" ? "Pies" : "Kot"
+                    }%0D%0AZachowanie: ${
+                      filters?.petBehavior === "averageBehavior"
+                        ? "Średniak"
+                        : filters?.petBehavior === "lazyBehavior"
+                        ? "Leniuch"
+                        : filters?.petBehavior === "crazyBehavior"
+                        ? "Wariat"
+                        : "Nie wybrano"
+                    }%0D%0AOpis: ${
+                      filters?.petDescription
+                        ? filters?.petDescription
+                        : "Nie podano opisu"
+                    }`}
+                  >
+                    Zapytaj o dostępność
+                  </a>
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         ))
